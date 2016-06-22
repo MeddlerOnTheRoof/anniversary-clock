4376f6950400b2b951c6e02485073407d2d612b1
MeddlerOnTheRoof <schanne.mj@gmail.com>
2016-06-22 17:27:33 -0400

    adding base clock changes with slideshow of dumb pictures plus some styling and waffing with docs

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # anniversary-clock
 A clock that counts up how long I've been with my girlfriend and has a slide of pictures.
+
+# Enhancement Ideas
+- integrate with dropbox for retrieving pictures
+- swipe left/right touch (arrows with hover highlights on desktop) for moving through pictures
+- swipe down (arrow with hover highlight on desktop) for accessing music controls, integrated with pandora
+- UWP version for windows
+- random text for random days (e.g. 'oh you look great today!')
+- shuffle mode for pictures (actually decent randomization without back-to-backs)
+- toggle playful and or sexual banter
+- input menu for configuration or administrative tasks so the user doesn't have to go into the code
+- quick admin menu from swipe up
+- double tap to pause music/picture slide
+- integrating with something similar to wegupp?
+- alternative integration options for different music or picture sources 
+
+
+http://codepen.io/nancita/pen/OXbEEg
--- public/core.js
+++ public/core.js
@@ -1,5 +1,73 @@
-var app = angular.module('anniversary-clock', []);
+const MILLISECONDS_TO_SECONDS = 1000;
+const MILLISECONDS_TO_MINS = MILLISECONDS_TO_SECONDS * 60;
+const MILLISECONDS_TO_HOURS = MILLISECONDS_TO_MINS * 60;
+const MILLISECONDS_TO_DAYS = MILLISECONDS_TO_HOURS * 24;
+const MILLISECONDS_TO_WEEKS = MILLISECONDS_TO_DAYS * 7;
+
+const pictures = [
+    './imgs/background.jpg',
+    './imgs/alt1.jpg',
+    './imgs/alt2.jpg',
+    './imgs/alt3.jpg'
+];
+
+var app = angular.module('clock', []);
 
 app.controller('mainController', ['$scope', function($scope){
-    
+    const anniversary = new Date(2014,8,4, 16, 0, 0, 0);
+    var counter = 0;
+
+    $scope.clock;
+
+    $scope.tick = function () {
+        var today = new Date();
+        var delta = today - anniversary;
+
+        var d = Math.floor(delta / MILLISECONDS_TO_DAYS);
+        delta -= d * MILLISECONDS_TO_DAYS
+
+        var h = Math.floor(delta / MILLISECONDS_TO_HOURS);
+        delta -= h * MILLISECONDS_TO_HOURS;
+
+        var m = Math.floor(delta / MILLISECONDS_TO_MINS);
+        delta -= m * MILLISECONDS_TO_MINS;
+
+        var s = Math.floor(delta / MILLISECONDS_TO_SECONDS);
+
+        $scope.clock = `${d} days, ${h} hours, ${m} minutes, and ${s} seconds (~${today.getFullYear() - anniversary.getFullYear()} years)`;
+
+        $scope.anniversaryToday = today.getYear() === anniversary.getYear() &&
+           today.getMonth() === anniversary.getMonth() &&
+           today.getDay() === anniversary.getDay();
+
+        counter++;
+        if(counter === 90000){
+            var img = document.getElementById('img'),
+            style = img.currentStyle || window.getComputedStyle(img, false),
+            currUrl = style.backgroundImage.slice(4, -1);
+
+             currUrl = style.backgroundImage.slice(4, -1).replace(/"/g, "").replace("http://localhost:8642/","./");
+
+            var ind = pictures.indexOf(currUrl);
+            if (ind !== pictures.length -1){
+                ind++;
+            } else {
+                ind = 0;
+            }
+
+            currUrl = pictures[ind].replace("./","http://localhost:8642/");
+
+            document.getElementById('img').style.backgroundImage = `url(${currUrl})`;
+
+            counter = 0;
+        }
+
+        // iterate through
+
+        $scope.$apply();
+    };
+
+    $scope.anniversaryToday = false;
+
+    setInterval($scope.tick, 500);
 }]);
